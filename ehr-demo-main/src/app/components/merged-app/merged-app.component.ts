71d63042828f14dce23c4bf508f6f95a1971c503
jyothisai <jkarumajji@edvak.com>
2025-02-14 15:50:24 +0530

    components created

---
+++ ehr-demo-main/src/app/components/merged-app/merged-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merged-app',
+  imports: [],
+  templateUrl: './merged-app.component.html',
+  styleUrl: './merged-app.component.scss'
+})
+export class MergedAppComponent {
+
+}
---
+++ ehr-demo-main/src/app/components/user-creation/user-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-creation',
+  imports: [],
+  templateUrl: './user-creation.component.html',
+  styleUrl: './user-creation.component.scss'
+})
+export class UserCreationComponent {
+
+}
