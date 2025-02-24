eff67995f36169551525ab9b9bbbd1cd8567edd5
pravallika <pbuddha@edvak.com>
2025-02-24 15:16:39 +0530

    In registration component underscore is added to password field

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.get('/dbUsers', authenticateToken, getAllUsers);//all existing users in database
 router.get('/pusers', authenticateToken, getUsersPag);//users who are not deleted and pagination
 router.post('/', authenticateToken, createUser);
-router.get('/', authenticateToken, getUsers);//users who are not deleted
+router.get('/', authenticateToken, getUsers);//Active-users who are not deleted
 router.get('/:id', authenticateToken, getUserById);
 router.put('/:id', authenticateToken, updateUser);
 router.delete('/:id', authenticateToken, deleteUser);
